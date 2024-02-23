80124d3dc265b5042f4aa851d24a2ee19e8a95c0
hankyungeun <az2282@naver.com>
2024-02-23 16:17:23 +0900

    jdbc관련 파일 추가

---
+++ app/src/main/java/anime_list/config/JDBC.java
@@ -0,0 +1,90 @@
+package anime_list.config;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class JDBC {
+    public static Connection getConnection() {
+        Properties prop = new Properties();
+
+        Connection conn = null; // DB연결
+
+        try {
+            prop.load(new FileInputStream(
+                    "/resources/driver.properties"));
+
+            Class.forName(prop.getProperty("driver"));
+
+            conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("username"),
+                    prop.getProperty("passwd"));
+            conn.setAutoCommit(false);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static void commit(Connection conn) {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.commit();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void rollback(Connection conn) {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.rollback();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void close(Statement stmt) {
+        try {
+            if (stmt != null && !stmt.isClosed()) {
+                stmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void close(Connection conn) {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void close(ResultSet rset) {
+        try {
+            if (rset != null && !rset.isClosed())
+                rset.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
