52e7ac9649d19812221ec403917bc07e9590ba94
Leedonghyeob <m715800@naver.com>
2024-03-05 19:26:19 +0900

    comment.dao만드는중

--- app/src/main/java/anime_list/model/dao/CommentDao.java
+++ app/src/main/java/anime_list/model/dao/CommentDao.java
@@ -1,5 +1,70 @@
 package anime_list.model.dao;
 
+
+import anime_list.model.vo.Comment;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import java.util.ArrayList;
+import java.util.InvalidPropertiesFormatException;
+import java.util.Properties;
+
 public class CommentDao {
 
+    private Properties prop = new Properties(); 
+
+    public CommentDao(){
+        
+      
+            try {
+                prop.loadFromXML(new FileInputStream("app/src/main/resources/db/commentQuery.xml"));
+            } catch (InvalidPropertiesFormatException e) {                
+                e.printStackTrace();
+            } catch (FileNotFoundException e) {               
+                e.printStackTrace();
+            } catch (IOException e) {                
+                e.printStackTrace();
+            } 
+    }       
+    
+    public ArrayList<Comment> selectAllList(Connection conn){
+        ArrayList<Comment> list = new ArrayList<>();
+
+        PreaparedStatement pstmt = null;
+        return list;
+    }
+
+   
+        
+        
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
