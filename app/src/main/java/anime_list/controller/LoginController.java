ba55be2e1159b9eab55ee0529ba9cb3548e599f2
Ryuyeonwoo <dusdn5218534@gmail.com>
2024-03-13 15:37:38 +0900

    로그인 / 회원가입 기능 완료

--- app/src/main/java/anime_list/controller/LoginController.java
+++ app/src/main/java/anime_list/controller/LoginController.java
@@ -1,49 +1,44 @@
 package anime_list.controller;
 
 import java.io.IOException;
-import java.util.UUID;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import javax.servlet.http.HttpSession;
 
 import anime_list.model.vo.User;
 import anime_list.service.UserService;
 
-@WebServlet("/insertUesr")
-public class UserController extends HttpServlet {
-
+@WebServlet("/loginUser")
+public class LoginController extends HttpServlet{
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         request.setCharacterEncoding("UTF-8");
         response.setContentType("text/plain; charset=UTF-8");
 
         // 폼에서 전달된 값 가져오기
-        String userPk = UUID.randomUUID().toString();
         String userId = request.getParameter("userId");
         String passwd = request.getParameter("passwd");
-        String name = request.getParameter("name");
 
         // User 객체 생성
-        User user = new User(userPk, userId, passwd, name);
-
-        // UserService를 이용하여 사용자 추가
-        int result = new UserService().insertUser(user);
-        System.out.printf("result: %d\n", result);
-        if (result > 0) {
-
-            System.out.println("회원 추가에 성공하셨습니다 SUCCESS!");
+        User loginUser = new UserService().loginUser(userId, passwd);
+        
+        if (loginUser == null) {
+            System.out.println("로그인 실패 !");
+            response.sendRedirect("/login");
+           
+		} else {
+            System.out.println("로그인 성공 SUCCESS!");
+            HttpSession session = request.getSession();
+			session.setAttribute("loginUser", loginUser);
             response.sendRedirect("/main");
-
-        } else {
-            System.out.println("회원 추가에 실해하셨습니다 Failed!!");
-            response.sendRedirect("/newuser");
-
-        }
+		}
+       
+       
     }
 
+     
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
@@ -54,4 +49,6 @@ public class UserController extends HttpServlet {
         System.out.println("doPost 메소드 실행!");
         doGet(request, response);
     }
+
+    
 }
