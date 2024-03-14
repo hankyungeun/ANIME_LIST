0639cf07cc303cf3e8b934f612f78e82a31bd935
hankyungeun <az2282@naver.com>
2024-03-14 12:21:15 +0900

    한글 깨짐 문제 수정

--- app/src/main/java/anime_list/servlet/AllListServlet.java
+++ app/src/main/java/anime_list/servlet/AllListServlet.java
@@ -1,11 +1,7 @@
 package anime_list.servlet;
 
-import anime_list.model.vo.User;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import java.io.IOException;
 
-import javax.servlet.Servlet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,10 +9,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import anime_list.model.vo.User;
 
 @WebServlet("/allAniList")
 public class AllListServlet extends HttpServlet {
@@ -38,7 +36,7 @@ public class AllListServlet extends HttpServlet {
         ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(context);
 
         templateResolver.setTemplateMode("XHTML");
-
+        templateResolver.setCharacterEncoding("UTF-8");
         templateResolver.setPrefix("/templates/");
         templateResolver.setSuffix(".html");
         templateResolver.setCacheTTLMs(3600000L);
@@ -49,24 +47,25 @@ public class AllListServlet extends HttpServlet {
         templateEngine.process("allAniList", ctx, response.getWriter());
 
         ///////////////////////////////////////////////////////////////////////////
-//        request.setCharacterEncoding("UTF-8");
-//        response.setContentType("text/html; charset=UTF-8");
-//        PrintWriter out = response.getWriter();
-//
-//        try {
-//            InputStream inputStream = getServletContext().getResourceAsStream("/templates/allAniList.html");
-//            InputStreamReader reader = new InputStreamReader(inputStream, "UTF-8");
-//
-//            char[] buffer = new char[1024];
-//            int bytesRead;
-//            while ((bytesRead = reader.read(buffer)) != -1) {
-//                out.write(buffer, 0, bytesRead);
-//            }
-//
-//            inputStream.close();
-//            reader.close();
-//        } catch (NullPointerException e) {
-//            response.sendRedirect(request.getContextPath() + "/error");
-//        }
+        // request.setCharacterEncoding("UTF-8");
+        // response.setContentType("text/html; charset=UTF-8");
+        // PrintWriter out = response.getWriter();
+        //
+        // try {
+        // InputStream inputStream =
+        // getServletContext().getResourceAsStream("/templates/allAniList.html");
+        // InputStreamReader reader = new InputStreamReader(inputStream, "UTF-8");
+        //
+        // char[] buffer = new char[1024];
+        // int bytesRead;
+        // while ((bytesRead = reader.read(buffer)) != -1) {
+        // out.write(buffer, 0, bytesRead);
+        // }
+        //
+        // inputStream.close();
+        // reader.close();
+        // } catch (NullPointerException e) {
+        // response.sendRedirect(request.getContextPath() + "/error");
+        // }
     }
 }
--- app/src/main/java/anime_list/servlet/LoginServlet.java
+++ app/src/main/java/anime_list/servlet/LoginServlet.java
@@ -1,14 +1,6 @@
 package anime_list.servlet;
 
-import anime_list.model.vo.User;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
-
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -17,6 +9,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import anime_list.model.vo.User;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
@@ -43,36 +41,38 @@ public class LoginServlet extends HttpServlet {
             ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(context);
 
             templateResolver.setTemplateMode("XHTML");
-
+            templateResolver.setCharacterEncoding("UTF-8");
             templateResolver.setPrefix("/templates/");
             templateResolver.setSuffix(".html");
             templateResolver.setCacheTTLMs(3600000L);
             TemplateEngine templateEngine = new TemplateEngine();
             templateEngine.setTemplateResolver(templateResolver);
-            WebContext ctx = new WebContext(request, response, getServletConfig().getServletContext(), request.getLocale());
+            WebContext ctx = new WebContext(request, response, getServletConfig().getServletContext(),
+                    request.getLocale());
 
             templateEngine.process("login", ctx, response.getWriter());
 
             ///////////////////////////////////////////////////////////////////
-//            request.setCharacterEncoding("UTF-8");
-//            response.setContentType("text/html; charset=UTF-8");
-//            PrintWriter out = response.getWriter();
-//
-//            try {
-//                InputStream inputStream = getServletContext().getResourceAsStream("/templates/login.html");
-//                InputStreamReader reader = new InputStreamReader(inputStream, "UTF-8");
-//
-//                char[] buffer = new char[1024];
-//                int bytesRead;
-//                while ((bytesRead = reader.read(buffer)) != -1) {
-//                    out.write(buffer, 0, bytesRead);
-//                }
-//
-//                inputStream.close();
-//                reader.close();
-//            } catch (NullPointerException e) {
-//                response.sendRedirect(request.getContextPath() + "/error");
-//            }
+            // request.setCharacterEncoding("UTF-8");
+            // response.setContentType("text/html; charset=UTF-8");
+            // PrintWriter out = response.getWriter();
+            //
+            // try {
+            // InputStream inputStream =
+            // getServletContext().getResourceAsStream("/templates/login.html");
+            // InputStreamReader reader = new InputStreamReader(inputStream, "UTF-8");
+            //
+            // char[] buffer = new char[1024];
+            // int bytesRead;
+            // while ((bytesRead = reader.read(buffer)) != -1) {
+            // out.write(buffer, 0, bytesRead);
+            // }
+            //
+            // inputStream.close();
+            // reader.close();
+            // } catch (NullPointerException e) {
+            // response.sendRedirect(request.getContextPath() + "/error");
+            // }
         }
     }
 }
