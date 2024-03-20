7ad9c711d65d1d8f386b4ee876c4c661560fbef1
Ryuyeonwoo <dusdn5218534@gmail.com>
2024-03-20 15:59:32 +0900

    로그인 중복 체크

---
+++ app/src/main/java/anime_list/controller/idchController.java
@@ -0,0 +1,45 @@
+package anime_list.controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import anime_list.model.dto.UserDto;
+import anime_list.service.UserService;
+
+
+
+@WebServlet("/chId")
+public class idchController extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.setCharacterEncoding("UTF-8");
+        String userId = request.getParameter("name");
+         
+        System.out.println(userId);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        UserDto userdto = new UserService().idchUser(userId);
+        
+        if(userdto != null){
+            response.getWriter().print("");
+        }else{
+            response.getWriter().print(userdto);
+        }
+        
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+                request.setCharacterEncoding("UTF-8");
+ 
+        System.out.println("doPost 메소드 실행!");
+        doGet(request, response);
+    }
+}
--- app/src/main/resources/static/js/newuser.js
+++ app/src/main/resources/static/js/newuser.js
@@ -1,105 +1,114 @@
+
+function chId() {
+
+  const id = document.getElementById("login-id").value;
+  $.ajax({
+    type: 'POST',
+    url: '/chId',
+    data: { name: id },
+    dataType: 'json',
+    success: function (data, status, msg) {
+
+      alert("사용 가능한 아이디 입니다.");
+    },
+    error: function (error, status, msg) {
+      alert("중복된 아이디 입니다");
+    },
+  });
+}
+
+
 function checkId() {
-    const ch = /^[a-z0-9]+$/;
-    const id = document.getElementById("login-id").value;
-    document.getElementById("error").innerHTML = "";
+  const ch = /^[a-z0-9]+$/;
+  const id = document.getElementById("login-id").value;
+  document.getElementById("error").innerHTML = "";
 
-    
+  if (ch.test(id)) {
 
-    if (ch.test(id)) {
-
-      if (id.length < 5) {
-        document.getElementById("error").innerHTML = "규정에 맞지 않는 아이디입니다";
-
-      }else{
-        return true;
-      }
-
-    } else {
+    if (id.length < 5) {
       document.getElementById("error").innerHTML = "규정에 맞지 않는 아이디입니다";
-    } 
 
-
-
-  }
-
-  function checkPw() {
-    const ch = /^[ㄱ-ㅎㅏ-ㅣ가-힣]+$/;
-    const pw = document.getElementById("login-pw").value;
-    console.log(pw);
-    console.log(ch.test(pw));
-
-    if (ch.test(pw)) {
-      console.log(222);
-      document.getElementById("error2").innerHTML = "규정에 맞지 않는 비밀번호입니다";
-    } else if (pw.length >= 8) {
-      document.getElementById("error2").innerHTML = " ";
-      return true;
     } else {
-      document.getElementById("error2").innerHTML = "규정에 맞지 않는 비밀번호입니다";
-    }
-
-  }
-
-  function checkPw2() {
-    
-    const pw = document.getElementById("login-pw").value;
-    const pw2 = document.getElementById("login-pw2").value;
-    if (pw == pw2) {
-      document.getElementById("error3").innerHTML = " ";
       return true;
+    }
+
+  } else {
+    document.getElementById("error").innerHTML = "규정에 맞지 않는 아이디입니다";
+  }
+
+}
+
+function checkPw() {
+  const ch = /^[ㄱ-ㅎㅏ-ㅣ가-힣]+$/;
+  const pw = document.getElementById("login-pw").value;
+
+  if (ch.test(pw)) {
+    document.getElementById("error2").innerHTML = "규정에 맞지 않는 비밀번호입니다";
+  } else if (pw.length >= 8) {
+    document.getElementById("error2").innerHTML = " ";
+    return true;
+  } else {
+    document.getElementById("error2").innerHTML = "규정에 맞지 않는 비밀번호입니다";
+  }
+
+}
+
+function checkPw2() {
+
+  const pw = document.getElementById("login-pw").value;
+  const pw2 = document.getElementById("login-pw2").value;
+  if (pw == pw2) {
+    document.getElementById("error3").innerHTML = " ";
+    return true;
+  } else {
+    document.getElementById("error3").innerHTML = "비밀번호가 일치하지 않습니다";
+  }
+
+
+}
+
+
+function checkName() {
+  const name = document.getElementById("login-us").value;
+  if (name != null && name != "") {
+    return true;
+  } else {
+    return null;
+  }
+}
+
+function newuser(event) {
+  event.preventDefault();
+  if (checkId() == true && checkPw() == true && checkPw2() == true && checkName() == true) {
+    document.mForm.submit();
+
+
+  } else {
+
+    if (checkId() == null) {
+      document.getElementById("login-id").focus();
+      alert("잘못된 아이디입니다.");
+
+    } else if (checkPw() == null) {
+      document.getElementById("login-pw").focus();
+      alert("잘못된 비밀번호입니다.");
+
+    } else if (checkPw2() == null) {
+      document.getElementById("login-pw2").focus();
+      alert("비밀번호 체크가 다릅니다.");
+
+    } else if (checkName() == null) {
+      document.getElementById("login-us").focus();
+      alert("잘못된 이름입니다");
+
     } else {
-      document.getElementById("error3").innerHTML = "비밀번호가 일치하지 않습니다";
+      alert("잘못된 입력입니다");
     }
 
 
   }
 
+}
 
-  function checkName() {
-    const name = document.getElementById("login-us").value;
-    console.log(name);
-    if(name != null && name != ""){
-        return true;
-    }else{
-        return null;
-    }
-  }
 
-  function newuser(event){
-    event.preventDefault();
-    console.log("--------")
-    if (checkId()== true && checkPw() == true && checkPw2() == true && checkName() == true){
-       document.mForm.submit();
-      console.log("--------")
 
-      
-    } else{
-      
-      if(checkId() == null){
-        document.getElementById("login-id").focus();
-        alert("잘못된 아이디입니다.");
-
-      }else if(checkPw() == null){
-        document.getElementById("login-pw").focus();
-        alert("잘못된 비밀번호입니다.");
-
-      }else if(checkPw2() == null){
-        document.getElementById("login-pw2").focus();
-        alert("비밀번호 체크가 다릅니다.");
-
-      }else if(checkName() == null){
-        document.getElementById("login-us").focus();
-        alert("잘못된 이름입니다");
-
-      }else{
-        alert("잘못된 입력입니다");
-      }
-
-      
-    }
-
-  } 
-  
-
-  
- 
